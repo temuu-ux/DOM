149fe67eeff893e3031e892bf590fb2f1c87ad39
temuu-ux <temuulenb80@gmail.com>
2023-12-18 15:52:05 +0800

    school

--- addElement/script.js
+++ addElement/script.js
@@ -1,3 +1,5 @@
-document.getElementById("root").innerHTML =' <input type="text" class="test" />
-
-'
+function addBox() {
+  document.getElementById("box").innerHTML += "<button class=but></button>";
+  //   document.getElementById("box").innerHTML =
+  //     document.getElementById("box").innerHTML + "<button class=but><button>";
+}
---
+++ davtalt/script.js
@@ -0,0 +1,3 @@
+function addBox() {
+  document.getElementById("box").innerHTML += "<button class=but></button>";
+}
