9eb5f75150013810535923af27ca2990996877e5
Temuulen Batbileg <23LP8360@G401-CC0107.local>
2023-12-20 14:58:40 +0800

    zip

---
+++ gogo/script.js
@@ -0,0 +1,16 @@
+const gogo = [
+  {
+    image_url: "./1jpeg",
+    title:
+      "Газар хөдлөлтийн улмаас амиа алдсан хүний тоо нэмэгдэж магадгүй байна",
+    description:
+      "Даваа гаргийн шөнө Хятадын баруун хойд нутагт 5.9 магнитудын хүчтэй газар хөдлөлт болсон. Үүний улмаас амиа алдсан хүний тоо 127 хүрчээ.",
+    icone: "fa-solid fa-share`,fa-solid fa-clock",
+  },
+];
+// root
+const root = document.getElementById("root");
+//  cardDiv
+const cardDiv = document.createElement("div");
+cardDiv.setAttribute("class", "card");
+// image_url
